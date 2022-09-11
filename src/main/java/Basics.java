e085a0f341917753829616d0ae1af6075284c664
SanzSS <102633707+SanzSS@users.noreply.github.com>
2022-09-11 01:44:26 -0400

    Update Basics.java

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -102,7 +102,7 @@ public class Basics {
          * Current count: 0
          */
         for (int i = 10; i != 0; i--){
-            System.out.println("Current count: " + i.toString());
+            System.out.println("Current count: i");
         }
 
 
