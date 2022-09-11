93b61d3a2b333e783fe5fe673a72b816a5cb8743
SanzSS <102633707+SanzSS@users.noreply.github.com>
2022-09-11 01:14:14 -0400

    Update Basics.java

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World");
 
 
 
@@ -62,6 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,6 +101,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 0; i <= 10; i++){
+            System.out.println(i);
+        }
 
 
     }
@@ -141,7 +146,9 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
-
+        String[] temp = to_split.split(" ");
+        for (int i = 0; int != 8; i++){
+            ret.append(temp[i]);
         // Fill in the rest of the body here
 
         return ret.toString();
@@ -163,13 +170,18 @@ public class Basics {
      */
     public static int oddSum(int[] arr) {
         int current_sum = 0;
-
+        if (oddSum.length().equals(0)){
+            return 0;
+        }
         /* TODO (Task 5): Complete this method body using a for-loop.
          *                You can find the length of an array by using the
          *                .length attribute (e.g. arr.length)
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        for (int i = 1; i < oddSum.length(); i+=2){
+            current_sum += oddSum[i];
+        }
 
         return current_sum;
     }
